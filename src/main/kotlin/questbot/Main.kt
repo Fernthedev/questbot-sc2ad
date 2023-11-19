e20423f15bb797467237b22ad50705ba17cf7a3b
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2023-11-19 00:12:22 -0400

    Explicitly enable message intent

--- src/main/kotlin/questbot/Main.kt
+++ src/main/kotlin/questbot/Main.kt
@@ -3,6 +3,7 @@ package questbot
 import com.google.inject.Guice
 import org.javacord.api.DiscordApiBuilder
 import org.javacord.api.entity.activity.ActivityType
+import org.javacord.api.entity.intent.Intent
 import questbot.api.IBootstrap
 import questbot.modules.MainModule
 import java.io.File
@@ -13,6 +14,7 @@ fun main() {
     val token = File("./token.txt").readText()
 
     val api = DiscordApiBuilder().setToken(token)
+        .addIntents(Intent.MESSAGE_CONTENT, Intent.GUILD_MEMBERS)
         .login().join()
 
     api.updateActivity(ActivityType.CUSTOM, "Finishing Flamingo🦩")
